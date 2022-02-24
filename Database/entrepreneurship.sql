d224ac2d2c3c1050a5726cedec85c9701626fd3b
Chathuri <cathambugala@gmail.com>
2022-02-25 00:04:26 +0530

    Aded the database

---
+++ Database/entrepreneurship.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2022 at 07:23 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `entrepreneurship`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rejistration`
+--
+
+CREATE TABLE `rejistration` (
+  `First_Name` int(30) NOT NULL,
+  `Last_Name` varchar(30) NOT NULL,
+  `Age` int(3) NOT NULL,
+  `Email` varchar(20) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `ContactNo` varchar(14) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `RePassword` varchar(20) NOT NULL,
+  `Username` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
