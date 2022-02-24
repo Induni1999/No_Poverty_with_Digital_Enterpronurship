0772311543a1a8b8b8cf5f6031dc09c594b985be
Chathuri <cathambugala@gmail.com>
2022-02-24 22:19:40 +0530

    Add php part to forgot password page

---
+++ Forgot password page.php
@@ -0,0 +1,37 @@
+<?php include('server.php') ?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="Forgot password  page.css" type="text/css">
+</head>
+<body>
+<form method="POST" action="Forgot password page.php">
+<div class="main">
+<div class="Fp">
+	<h2>Frogot your password</h2>
+	<br>
+	<br>
+	<br>
+	<br>
+	<label>Enter your user name </label>
+	<br>
+	<input type="name" name="name" id="name" placeholder="Enter Your user name">
+	<br>
+	<br>
+	<label>Create new password </label>
+	<br>
+	<input type="password" name="name" id="name" placeholder="Create New password">
+	<br>
+	<br>
+	<label>Re-Enter new  password  </label>
+	<br>
+	<input type="password" name="password" id="name" placeholder="Re-Enter new password">
+	<br>
+	<br>
+	<br>
+	<input type="submit" value="Update Password" name="submit" id="submit">
+	<br>
+	<input type="clear" value="clear" name="clear" id="submit">
+</form>
+</body>
+</html>	
